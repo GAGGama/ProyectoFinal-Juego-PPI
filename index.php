1277ce8938d968013fba371ade1bc6519b4e0f02
GAGGama <alberto.alarcon@alumnos.udg.mx>
2021-06-07 17:40:17 -0500

    Prueba 1

---
+++ index.php
@@ -0,0 +1,96 @@
+<html>
+
+	<head>
+		<!-- <title>Formulario request</title>
+		<script src="js/jquery-3.3.1.min.js"></script> -->
+	</head>
+	<!-- <script>
+		function recibe(){
+			var correo = document.forma01.correo.value;		
+			var pass = document.forma01.pass.value;
+			if(correo=="" || pass==""){
+				$('#mensaje').html('Faltan campos por llenar');
+				setTimeout("$('#mensaje').html('')",5000);
+			}else{
+				$.ajax({
+					url:		'./Funciones/verfica_datos.php',
+					type:		'post',
+					data: $("#forma01").serialize(),
+					success:	function(res){
+						if(res==0){
+							$('#mensaje').html('Usuario no encontrado');
+							setTimeout("$('#mensaje').html('')",5000);
+						}else{
+							document.forma01.method='post';
+							document.forma01.action='bienvenido.php';
+							document.forma01.submit();
+						}
+					},
+					error:  function(){
+						alert('Error al conectar al servidor...');
+					}
+				});
+				
+			}
+		}
+	</script> -->
+	<style>
+		body{
+			background-image:url("imagenes/Fondo5.JPG");
+		}
+		
+		#formulario{
+			background:#FFFFFF;
+			position: absolute; /* podría ser relative */
+			width: 300px;
+			height: 300px;
+			left: 35%;
+			top: 150px;
+			border: groove 1em #A9A4A4;  
+			border-radius: 2em;
+		}
+		form label{
+			width:72px;
+			font-weight:bold;
+			display:inline-block;
+		}
+		form input[type="text"], form input[type="password"]{
+			width:180px;
+			padding:3px 10px;
+			border:1px solid #f6f6f6;
+			border-radius:3px;
+			background-color:#f6f6f6;
+			margin:8px 0;
+			display:inline-block;
+		}
+		form input[type="submit"]{
+			width:90%;
+			padding:8px 16px;
+			margin-top:32px;
+			border:1px solid #000;
+			border-radius:5px;
+			display:block;
+			color:#fff;
+			background-color:#000;
+		}
+		form input[type="submit"]:hover{
+			color: #0B86C5;
+			background-color: #ffffff;
+		}		
+	</style>
+	
+ <body name="Padre">
+	<div id="formulario" align="center">
+		<form name="forma01" id="forma01" action="lista_administradores_tabla.php" method="POST">
+			<label>Correo:</label>
+			<input type="text" name="correo" id="correo" placeholder="Escribe tu correo"/>
+			<br>
+			<label for="pasw">Contraseña:</label>
+			<input type="password" id="pass" name="pass" placeholder="Escribe tu Contraseña"/>
+			<br>
+			<input onclick="recibe(); return false;" type="submit" value="Ingresar"/>
+			<div id="mensaje" class="error"></div>
+		</form>
+	</div>
+ </body>
+</html>
