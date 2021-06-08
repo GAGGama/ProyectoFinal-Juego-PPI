4fc84801b43cef201430456c4f90bbe81dfed166
GAGGama <alberto.alarcon@alumnos.udg.mx>
2021-06-07 20:02:03 -0500

    Prueba 3

--- funciones/verifica_nickname.php
+++ funciones/verifica_nickname.php
@@ -4,21 +4,21 @@
 	require "conecta.php";
 	$con=conecta();
 	//Recibe variables
-	$correo = $_POST['correo'];
-    $contrasena  = $_POST['pass'];
-	$num	=0;
-	$contrasena = md5($contrasena);
+	// $correo = $_POST['correo'];
+    // $contrasena  = $_POST['pass'];
+	 $num	=0;
+	// $contrasena = md5($contrasena);
 	
-	$sql="SELECT * FROM administradores WHERE correo='$correo' AND pass='$contrasena' AND status=1 AND eliminado=0";
-	$res=mysql_query($sql, $con);
-	$num=mysql_num_rows($res);
+	// $sql="SELECT * FROM administradores WHERE correo='$correo' AND pass='$contrasena' AND status=1 AND eliminado=0";
+	// $res=mysql_query($sql, $con);
+	// $num=mysql_num_rows($res);
 	
-	if($num){
-		$idU 	= mysql_result($res,0,"id");
-		$nombre	=mysql_result($res,0,"nombre").' '.mysql_result($res,0,"apellidos");
-		$_SESSION['idU']	= $idU;
-		$_SESSION['nombre']	=$nombre;
-	}
+	// if($num){
+	// 	$idU 	= mysql_result($res,0,"id");
+	// 	$nombre	=mysql_result($res,0,"nombre").' '.mysql_result($res,0,"apellidos");
+	// 	$_SESSION['idU']	= $idU;
+	// 	$_SESSION['nombre']	=$nombre;
+	// }
 	
 	echo $num;
 ?>
