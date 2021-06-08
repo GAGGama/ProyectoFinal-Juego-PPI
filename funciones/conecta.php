cd3e76b45bc522d31a6b1bdcc3aa84c486ec50d7
GAGGama <alberto.alarcon@alumnos.udg.mx>
2021-06-07 21:56:26 -0500

    Prueba 3

--- funciones/conecta.php
+++ funciones/conecta.php
@@ -1,11 +1,11 @@
 <?php
 	
-	/*define ("HOST", 'us-cdbr-east-04.cleardb.com');
+	define ("HOST", 'us-cdbr-east-04.cleardb.com');
 	define ("BD", 'heroku_cb0cc88446c27b8');
 	define ("USER_BD", 'b5e289d6409ef9');
 	define ("PASS_BD", '5e49ca09');
 
-	function conecta(){
+	/*function conecta(){
 		if(!($con=mysql_connect(HOST,USER_BD,PASS_BD))){
 			echo "Error conectando al servidor de BBDD";
 			exit();		
